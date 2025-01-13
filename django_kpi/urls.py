d6973163685fb45dec7d01b0ac620a1abaca976e
mchahboun <mchahboun@majaracapital.com>
2025-01-13 19:16:39 +0000

    improved kpi urls

--- django_kpi/urls.py
+++ django_kpi/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('kpi.urls')),
+    path('kpi/', include('kpi.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- kpi/urls.py
+++ kpi/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'kpi'
 
 urlpatterns = [
-    path('kpi/get-model-fields/', views.get_model_fields, name='get_model_fields'),
-    path('kpi/get-field-values/', views.get_field_values, name='get_field_values'),
+    path('get-model-fields/', views.get_model_fields, name='get_model_fields'),
+    path('get-field-values/', views.get_field_values, name='get_field_values'),
 ]
