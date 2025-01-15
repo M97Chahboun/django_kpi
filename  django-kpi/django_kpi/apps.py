6ae8b20b1bb50b13fde42fa5286b0ffcbec6be44
Mohammed <m97.chahboun@gmail.com>
2025-01-15 18:30:40 +0100

    published beta version

---  django-kpi/django_kpi/apps.py
+++  django-kpi/django_kpi/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class KpiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "kpi"
+    name = "django_kpi"
+    label = "kpi"
